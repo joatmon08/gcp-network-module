06c0a2bde4b7a5710aa62388d1627b1023750d8d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-04-19 20:55:04 -0400

    Autopep8 formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='network',
-   version='1.0',
-   description='Factory module for GCP network',
-   author='Rosemary Wang',
-   author_email='915624+joatmon08@users.noreply.github.com',
-   packages=find_packages('.'),
+    name='network',
+    version='1.0',
+    description='Factory module for GCP network',
+    author='Rosemary Wang',
+    author_email='915624+joatmon08@users.noreply.github.com',
+    packages=find_packages('.'),
 )
